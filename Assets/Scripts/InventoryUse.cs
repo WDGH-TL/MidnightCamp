40859110dd600eeea2875d3c662d58ab2f30be35
WDGH-TL <williamdantegarcia@gmail.com>
2025-12-05 19:20:26 -0300

    UI for crafting

---
+++ Assets/Scripts/InventoryUse.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class InventoryUse : MonoBehaviour
+{
+    public void interact()
+    {
+        Debug.Log("Interacting with an item");
+    }
+
+    public void closeTab()
+    {
+        Debug.Log("Closing Tab");
+    }
+}
