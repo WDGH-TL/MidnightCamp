38286c8e437b682afcafae933f3f2a2269e6792e
WDGH-TL <williamdantegarcia@gmail.com>
2025-12-05 19:48:36 -0300

    Interaction & Crafting Menu WIP

--- Assets/Scripts/InventoryUse.cs
+++ Assets/Scripts/InventoryUse.cs
@@ -2,13 +2,55 @@ using UnityEngine;
 
 public class InventoryUse : MonoBehaviour
 {
+    public GameObject menuDesplegable;
+    public GameObject menuCrafteo;
+
+
+    public void Start()
+    {
+        menuDesplegable.SetActive(false);
+        menuCrafteo.SetActive(false);
+    }
+
     public void interact()
     {
         Debug.Log("Interacting with an item");
+        showMenu();
     }
 
     public void closeTab()
     {
         Debug.Log("Closing Tab");
+        HideMenu();
+    }
+    public void showMenu()
+    {
+
+        if (menuDesplegable != null)
+        {
+
+            menuDesplegable.SetActive(true);
+
+        }
+
+    }
+
+
+    public void HideMenu()
+    {
+        if (menuDesplegable != null)
+        {
+            menuDesplegable.SetActive(false);
+        }
+    }
+    public void showMenuCraft()
+    {
+
+        if (menuCrafteo != null)
+        {
+
+            menuCrafteo.SetActive(true);
+        }
+
     }
 }
